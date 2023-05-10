e538d24921ddc25753cd4ec717b0990278a50afd
Nilesoj20 <jvrn2014@gmail.com>
2023-05-10 10:00:22 -0700

    fix: errors in the standard error and data type

--- task0.c
+++ task0.c
@@ -9,7 +9,28 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	char *valor = strtok(NULL, " \n");
 	stack_t *nuevo = malloc(sizeof(stack_t));
+	int i;
 
+	if (valor)
+	{
+		for (i = 0; valor[i]; i++)
+		{
+			if ((valor[i] >= 48 && valor[i] <= 57) || valor[i] == '-')
+			{
+				nuevo->n = atoi(valor);
+				nuevo->prev = NULL;
+				nuevo->next = *stack;
+				if (*stack)
+					(*stack)->prev = nuevo;
+				*stack = nuevo;
+			}
+			else
+			{
+				fprintf(stderr, "L%u: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 	if (valor == NULL)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
@@ -20,12 +41,6 @@ void push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	nuevo->n = atoi(valor);
-	nuevo->prev = NULL;
-	nuevo->next = *stack;
-	if (*stack)
-		(*stack)->prev = nuevo;
-	*stack = nuevo;
 }
 
 /**
