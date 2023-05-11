d92080017129531cf425fce0f6133dd99e93b271
Nilesoj20 <jvrn2014@gmail.com>
2023-05-10 20:49:47 -0700

    fix: add 2 functions pop and pint

--- monty.h
+++ monty.h
@@ -40,4 +40,5 @@ extern instruction_t instructions[];
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 #endif /*manty.h*/
