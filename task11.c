60c7136c3be612e36c21f6b12deaa342de7c81e6
Nilesoj20 <jvrn2014@gmail.com>
2023-05-11 18:01:11 -0700

    fix: validate that the value of stack is NULL

--- task11.c
+++ task11.c
@@ -7,7 +7,7 @@
   */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (!stack)
+	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
